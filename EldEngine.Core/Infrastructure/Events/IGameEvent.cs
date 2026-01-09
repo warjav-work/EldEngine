3a391d4f1c34e4bad8333ab02e14e002370abac7
DESKTOP-UKDCDPL\usuario <warjav.work@gmail.com>
2026-01-09 13:11:17 +0100

    Agregar bus de eventos

---
+++ EldEngine.Core/Infrastructure/Events/IGameEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace EldEngine.Core.Infrastructure.Events
+{
+    /// <summary>
+    /// Evento base que todos los eventos deben implementar.
+    /// </summary>
+    public interface IGameEvent
+    {
+        string EventName { get; }
+    }
+}
